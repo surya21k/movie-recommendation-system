8de8bec853949d204f08640d342dcf5b2837da65
Surya_kiran <kataboinasuryakiran@gmail.com>
2026-06-09 22:50:26 +0530

    Load MovieLens dataset and setup EDA notebook

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello! World") 
+movies = ["Interstellar", "Inception", "Avatar"]
+
+print("First Movie:", movies[0])
+print("Second Movie:", movies[1])
+
+movies.append("Titanic")
+
+print("Updated List:", movies)
+
+print("Number of Movies:", len(movies))
